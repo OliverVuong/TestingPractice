8d4b854da009496fb28d50e83bc8751ba2ceec42
Oliver Vuong <vuongoliver@gmail.com>
2023-09-21 22:30:37 -0700

    Add simple calculator and tests

--- practice.js
+++ practice.js
@@ -11,7 +11,19 @@ const reverseString = (str) => {
 }
 
 const calculatorFactory = () => {
-    
+    const add = (a, b) => {
+        return a + b;
+    }
+    const subtract = (a, b) => {
+        return a - b;
+    }
+    const divide = (a, b) => {
+        return a / b;
+    }
+    const multiply = (a, b) => {
+        return a * b;
+    }
+    return {add, subtract, divide, multiply };
 }
 
 const caesarCipher = () => {
--- practice.test.js
+++ practice.test.js
@@ -25,3 +25,19 @@ test('reverse balthazar', () => {
 test('reverse special character', () => {
     expect(reverseString(' ?!@#123')).toBe('321#@!? ');
 })
+
+describe('Calculator', () => {
+    let calculator = calculatorFactory();
+    test('add 2 and 3 equals 5', () => {
+        expect(calculator.add(2, 3)).toBe(5);
+    })
+    test('5 minus 2 equals 3', () => {
+        expect(calculator.subtract(5, 2)).toBe(3);
+    })
+    test('divide 8 by 4 equals 2', () => {
+        expect(calculator.divide(8, 4)).toBe(2);
+    })
+    test('multiply 3 and 7 equals 21', () => {
+        expect(calculator.multiply(3, 7)).toBe(21);
+    })
+})
